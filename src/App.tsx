7fab9e71a2733272726dd902b66284079084404c
Binh Pham <binh.pham@synaigy.com>
2024-05-07 15:14:47 +0200

    feat: add function as property of ListGroup component

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,15 @@ import ListGroup from "./components/ListGroup";
 function App() {
    const items = ["Ha Noi", "Berlin", "Dortmund", "Köln", "Düsseldorf"];
 
+   const handleSelectItem = (item: string) => {
+      console.log(item);
+   };
+
    return (
       <ListGroup
          items={items}
          heading="Listtt"
+         onSelectItem={handleSelectItem}
       />
    );
 }
